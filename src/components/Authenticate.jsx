008751e358dcea8dd1dc7fbfeb7ae61e8ecf8dc0
hh0122 <huynh.vu2201@gmail.com>
2024-11-01 22:28:07 -0500

    Update Authenticate.jsx

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
 
+export default function Authenticate({ token }) {
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
 
-
-export default function Authenticate (){
-  const [error, setError] = useState(null)
-  const [successMessage, setSuccessMessage] = useState(null)
   async function handleClick() {
-    try{
-      const response = await fetch(`https://fsa-jwt-practice.herokuapp.com/authenticate`,
-      {
-      method: "GET",
-      headers : {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
-  const result = await response.json();
-  setSuccessMessage(result.message);
-    }catch (error){
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const result = await response.json();
+      setSuccessMessage(result.message);
+    } catch (error) {
       setError(error.message);
     }
-    console.log(`what`)
   }
-  return (
-    <>
-    <h2> Authenticate! </h2>
-    {error && <p>{error}</p>}
-    <button onClick={handleClick}> Authenticate Token </button>
-    
-    </>
 
-  )
+  return (
+    <div>
+            <h2>Authenticate</h2>
+            {successMessage && <p>{successMessage}</p>}
+            {error && <p>{error}</p>}
+            <button onClick={handleClick}>Authenticate Token!</button>
+          
+    </div>
+  );
 }
